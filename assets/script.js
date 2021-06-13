2c8254b204be42acd6d0bc57c3c4d2b8335371ec
BA1bers <brent.albers@yahoo.com>
2021-06-13 13:05:33 -0700

    set up html, set up grid layout for javascript

---
+++ assets/script.js
@@ -0,0 +1,45 @@
+var APIkey = 'd89681d01b91040ec62000543fb31e40';
+//Set up dom element references
+var city
+var temperature
+var humidity
+var windSpeed
+var UVindex
+
+//Set up function to display search history
+    //Within function, loop through search history array
+
+//create a function to update history within local storage
+
+//create a function that gets info from local storage
+
+//create a function that displays the current weather that I am fetching from open weather api
+    //Put all variables created at the top within this function
+    //create dom elements & set class attributes, include conditional flow using if else statements
+    //append everything to the correct things
+
+//create a function that displays the daily forecast
+    //set local variables, create dom elements, add content elements
+
+//create a function for a five day forecast
+    //create local variables, create dom references
+
+//create a function that renders everything (location, data, currrent weather)
+
+//create a function that fetches the weather location from weather geo location
+    //calls other functions to display current weather and five day forecast
+    //set local variables
+    //use fetch api to get data and render it using render function above
+
+//create a function that fetches the quordinates
+    //use fetch api
+
+//create a function that handles the search form submission
+    //use event.prevent default
+
+//create final function that handles the search history button
+    //use a bit of conditional flow within final two functions
+
+//call the function that gets the search history from local storage
+    //add event listener to search form including the handle search form submission function within it
+    //add event listener to the search history element container (this will call the function that handles the search history click event)
