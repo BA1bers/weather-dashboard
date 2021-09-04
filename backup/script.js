ff5afe08692efa87f364e9e2fb74fa540b0e80de
BA1bers <brent.albers@yahoo.com>
2021-09-03 18:10:43 -0700

    created function to display current weather, local storage now saves previous searches as selectable options on screen

--- backup/script.js
+++ backup/script.js
@@ -1,5 +1,6 @@
 var apiKey = 'd89681d01b91040ec62000543fb31e40';
 var cityWeather = {};
+// var city = document.getElementById('searchBar');
 var button = document.getElementById('searchButton');
 var cityName = document.getElementById('cityName');
 var temp = document.getElementById('temp');
@@ -8,24 +9,19 @@ var windSpeed = document.getElementById('windSpeed');
 var uvIndex = document.getElementById('uvIndex');
 var searchHistory = [];
 
-// Clears previous city when new city is searched
-function clearOneDayForecastSection() {
+
+function grabWeather(city) {
     $("#cityName").html("");
     $("#temp").html("");
     $("#humidity").html("");
     $("#windSpeed").html("");
     $("#uvIndex").html("");
-}
-
-// Fetches the weather information from the openweather api
-function grabWeather(city) {
-    clearOneDayForecastSection();
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=d89681d01b91040ec62000543fb31e40`)
         .then(response => response.json())
         .then(data => {
             console.log(data)
             cityName.append(data.name);
-            temp.append("Temperature: " + data.main.temp + "ºF");
+            temp.append("Temperature: " + data.main.temp);
             humidity.append("Humidity: " + data.main.humidity);
             windSpeed.append("Wind Speed: " + data.wind.speed);
             fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&exclude=minutely,hourly&appid=d89681d01b91040ec62000543fb31e40`).then(response => response.json())
@@ -33,74 +29,69 @@ function grabWeather(city) {
                     console.log(uvInfo)
                     uvIndex.append("UV Index: " + uvInfo.current.uvi);
                 })
-        });
-    var cityVal = $("#searchBar").val()
+            var cityVal = $("#searchBar").val();
 
-    //makeNewCityBttn(cityVal)
-    $("#searchBar").val("");
+            saveCities(cityVal)
+            $("#searchBar").val("");
+        });
+
 
     console.log(city.value)
 };
 
-// Saves new city entry into local storage, second part creates one new for each search
-function makeNewCityBttn(cityInfo) {
+
+
+
+
+function saveCities(cityVal) {
     if (searchHistory === null) {
         searchHistory = []
     }
-    console.log(cityInfo)
-    searchHistory.push(cityInfo)
+    console.log(cityVal)
+    searchHistory.push(cityVal)
     localStorage.setItem("cityVal", JSON.stringify(searchHistory))
-    //createCitiesOnFirstVisit()
-    let newCityBttn = document.createElement("input")
-    newCityBttn.setAttribute("class", "btn btn-secondary btn-sm")
-    newCityBttn.setAttribute("value", cityInfo)
-    newCityBttn.setAttribute("readonly", true)
-    // Giving each button click functionality
-    newCityBttn.addEventListener("click", function (event) {
-        event.preventDefault();
-        clearOneDayForecastSection();
-        grabWeather(newCityBttn.value)
-    })
-    $("#history").append(newCityBttn);
+    displayCities()
 }
 
-// The first function that gets called
-function createCitiesOnFirstVisit() {
-    // Clears anything inside history html element
+function displayCities() {
     $("#history").html("")
-    // Stores array to search history variable
     searchHistory = JSON.parse(localStorage.getItem("cityVal"))
-    // If search history had something saved, we pass the conditional into the for loop
     if (searchHistory != null) {
-        // For loop creating a button for items saved within search history
         for (let i = 0; i < searchHistory.length; i++) {
             const storedCity = document.createElement("input")
             storedCity.setAttribute("class", "btn btn-secondary btn-sm")
             storedCity.setAttribute("value", searchHistory[i])
             storedCity.setAttribute("readonly", true)
-            // Giving each button click functionality
-            storedCity.addEventListener("click", function (event) {
+            storedCity.addEventListener("click", function () {
                 event.preventDefault();
-                clearOneDayForecastSection();
+                $("#cityName").html("");
+                $("#temp").html("");
+                $("#humidity").html("");
+                $("#windSpeed").html("");
+                $("#uvIndex").html("");
                 grabWeather(storedCity.value)
             })
             $("#history").append(storedCity)
         }
-        clearOneDayForecastSection();
+        $("#cityName").html("");
+        $("#temp").html("");
+        $("#humidity").html("");
+        $("#windSpeed").html("");
+        $("#uvIndex").html("");
     }
 }
-
-// When the user uses the input box to search for a new city, it clears the previous search
 $(".searchCity").on("submit", function (event) {
     event.preventDefault();
     let city = $("#searchBar").val()
-    clearOneDayForecastSection();
-    console.log(city);
+    $("#cityName").html("");
+    $("#temp").html("");
+    $("#humidity").html("");
+    $("#windSpeed").html("");
+    $("#uvIndex").html("");
+    console.log(city)
     grabWeather(city);
-    makeNewCityBttn(city);
 })
-createCitiesOnFirstVisit()
-
+displayCities()
 // function clear() {
 //     $("#temp")
 // }
